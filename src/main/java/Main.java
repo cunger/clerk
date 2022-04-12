b1b3c18a9568eb7c2ddca111512d6191059e1d81
christina <andrea.christina.unger@gmail.com>
2022-04-12 08:24:17 +0200

    add: EA 1.5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,21 +13,35 @@ public class Main {
         );
 
         runHomework(
-            "List Scheduler with priority ordering",
+            "1.4 | List Scheduler with priority ordering",
             new ListScheduler(OrderingStrategy.PRIORITY),
-            taskQueue
+            taskQueue,
+            new int[] {2, 4, 8}
         );
 
         runHomework(
-            "List Scheduler with longest-runtime-first ordering",
+            "1.4 | List Scheduler with longest-runtime-first ordering",
             new ListScheduler(OrderingStrategy.LONGEST_RUNTIME_FIRST),
-            taskQueue
+            taskQueue,
+            new int[] {2, 4, 8}
+        );
+
+        TaskQueue imageProcessingTasks = new TaskQueue(1, 2, 2, 10, 6, 6, 1);
+
+        runHomework(
+            "1.5 | List Scheduler with longest-runtime-first ordering",
+            new ListScheduler(OrderingStrategy.LONGEST_RUNTIME_FIRST),
+            imageProcessingTasks,
+            new int[] {1, 4}
         );
     }
 
-    private static void runHomework(String headline, ListScheduler listScheduler, TaskQueue taskQueue) {
+    private static void runHomework(String headline, ListScheduler listScheduler, TaskQueue taskQueue, int[] resourcePools) {
         System.out.println("\n---- " + headline + " -----\n");
-        for (int numberOfResoures : new int[] {2, 4, 8}) {
+        System.out.println("Tasks with runtime: ");
+        taskQueue.forEach(task -> System.out.println(" " + task.id() + " (" + task.runtime() + ")"));
+        System.out.println("");
+        for (int numberOfResoures : resourcePools) {
             Schedule schedule = listScheduler.schedule(taskQueue, numberOfResoures);
             System.out.println(numberOfResoures + " resources");
             System.out.println(schedule.print());
